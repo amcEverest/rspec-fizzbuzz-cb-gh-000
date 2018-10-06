6e6de76fcc21cc152cb5779064fc2999ef8e110f
Andres Cortez <andres.corteztm@gmail.com>
2018-10-06 12:22:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-if num%3 == 0
-  print "Fizz"
-elsif num%5 == 0
-  print "Buzz"
-elsif num%3 == 0 && num%5 == 0
-  print "FizzBuzz"
-else
-  false
+def fizzbuzz(num)
+  if num%3 == 0
+    print "Fizz"
+  elsif num%5 == 0
+    print "Buzz"
+  elsif num%3 == 0 && num%5 == 0
+    print "FizzBuzz"
+  else
+    nil
+  end
+end
